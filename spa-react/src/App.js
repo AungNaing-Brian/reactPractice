075d8af58a03850cfb1115e779aacf86ee677feb
Aung Naing <aungnaing.brian@gmail.com>
2025-12-24 00:17:49 +0400

    save1

---
+++ spa-react/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import MYImages from './assets/image2.jpg';
+
+function App() {
+ let name = "Brian";
+
+ let changeName = () => {
+  name = "Aung Naing";
+  console.log(name);
+ }
+ return (
+  <div>
+    <h1>Hello {name}</h1>
+    <button onClick={changeName}>change name</button>
+  </div>
+ )
+//  return (
+//   <div className = "add">
+//     <h1>Hello{name}</h1>
+//     {/* 1st way - public folder way */}
+//     {/* absolute path */}
+//     {/* <img src="http://localhost:3000/image.jpg" /> */}
+
+//     {/* relative path */}
+//     {/* <img src="/image1.jpg" /> */}
+
+//     {/* 2nd way import */}
+
+//     {/* <img src={MYImages} /> */}
+//   </div>
+//  )
+}
+
+export default App;
